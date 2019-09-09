c834479db6d22b807b7e6abb552ad7180c20b3c9
RValner <valnerrobert@gmail.com>
2019-09-09 18:26:47 +0300

    Changed to counting filter

--- src/gesture_processing.cpp
+++ src/gesture_processing.cpp
@@ -21,6 +21,7 @@ public:
   {
     human_list_sub_ = nh_.subscribe("human_list", 1, &GestureRecognizer::humanListCallback, this);
     gesture_pub_ = nh_.advertise<std_msgs::Int16>("detected_gesture", 1);
+    command_history_.clear();
     ROS_INFO("Gesture Recognizer is good to go");
   }
 
@@ -39,42 +40,42 @@ private:
     if (msg.num_humans == 0)
     {
       // No humans were detected
-      average_gesture_code_ += double(-1)/max_count_;
+      command_history_[-1]++;
     }
     else if ((msg.human_list[0].body_key_points_with_prob[2].y > msg.human_list[0].body_key_points_with_prob[3].y) and (msg.human_list[0].body_key_points_with_prob[3].y > msg.human_list[0].body_key_points_with_prob[4].y) and (abs(msg.human_list[0].body_key_points_with_prob[5].y - msg.human_list[0].body_key_points_with_prob[6].y) < 50) and (abs(msg.human_list[0].body_key_points_with_prob[5].y - msg.human_list[0].body_key_points_with_prob[7].y) < 50))
     {
       // Forward + right
-      average_gesture_code_ += double(2)/max_count_;
+      command_history_[2]++;
     }
     else if ((msg.human_list[0].body_key_points_with_prob[5].y > msg.human_list[0].body_key_points_with_prob[6].y) and (msg.human_list[0].body_key_points_with_prob[6].y > msg.human_list[0].body_key_points_with_prob[7].y) and (abs(msg.human_list[0].body_key_points_with_prob[2].y - msg.human_list[0].body_key_points_with_prob[3].y) < 50) and (abs(msg.human_list[0].body_key_points_with_prob[2].y - msg.human_list[0].body_key_points_with_prob[4].y) < 50))
     {
       // Forward + left
-      average_gesture_code_ += double(4)/max_count_;
+      command_history_[4]++;
     }
     else if ((msg.human_list[0].body_key_points_with_prob[5].y < msg.human_list[0].body_key_points_with_prob[6].y) and (msg.human_list[0].body_key_points_with_prob[6].y < msg.human_list[0].body_key_points_with_prob[7].y) and (abs(msg.human_list[0].body_key_points_with_prob[2].y - msg.human_list[0].body_key_points_with_prob[3].y) < 50) and (abs(msg.human_list[0].body_key_points_with_prob[2].y - msg.human_list[0].body_key_points_with_prob[4].y) < 50))
     {
       // Left
-      average_gesture_code_ += double(5)/max_count_;
+      command_history_[5]++;
     }
     else if ((msg.human_list[0].body_key_points_with_prob[2].y < msg.human_list[0].body_key_points_with_prob[3].y) and (msg.human_list[0].body_key_points_with_prob[3].y < msg.human_list[0].body_key_points_with_prob[4].y) and (abs(msg.human_list[0].body_key_points_with_prob[5].y - msg.human_list[0].body_key_points_with_prob[6].y) < 50) and (abs(msg.human_list[0].body_key_points_with_prob[5].y - msg.human_list[0].body_key_points_with_prob[7].y) < 50))
     {
       // Right
-      average_gesture_code_ += double(1)/max_count_;
+      command_history_[1]++;
     }
     else if (((msg.human_list[0].body_key_points_with_prob[3].y > msg.human_list[0].body_key_points_with_prob[2].y) and (abs(msg.human_list[0].body_key_points_with_prob[2].y - msg.human_list[0].body_key_points_with_prob[4].y) < 40)) or ((msg.human_list[0].body_key_points_with_prob[6].y > msg.human_list[0].body_key_points_with_prob[5].y) and (abs(msg.human_list[0].body_key_points_with_prob[5].y - msg.human_list[0].body_key_points_with_prob[7].y) < 40)) and ((abs(msg.human_list[0].body_key_points_with_prob[2].y - msg.human_list[0].body_key_points_with_prob[3].y) > 50) or (abs(msg.human_list[0].body_key_points_with_prob[5].y - msg.human_list[0].body_key_points_with_prob[6].y) > 50)))
     {
       // Back
-      average_gesture_code_ += double(3)/max_count_;
+      command_history_[6]++;
     }
     else if (((msg.human_list[0].body_key_points_with_prob[2].y > msg.human_list[0].body_key_points_with_prob[3].y) and (msg.human_list[0].body_key_points_with_prob[3].y > msg.human_list[0].body_key_points_with_prob[4].y)) or ((msg.human_list[0].body_key_points_with_prob[5].y > msg.human_list[0].body_key_points_with_prob[6].y) and (msg.human_list[0].body_key_points_with_prob[6].y > msg.human_list[0].body_key_points_with_prob[7].y)))
     {
       // Forward
-      average_gesture_code_ += double(3)/max_count_;
+      command_history_[3]++;
     }
     else
     {
       // No recognizable commands
-      average_gesture_code_ += 0;
+      command_history_[0]++;
     }
 
     /*
@@ -82,11 +83,26 @@ private:
      */
     if (message_counter_ >= max_count_)
     {
-      gesture_code_msg_.data = std::round(average_gesture_code_);
+      gesture_code_msg_.data = getCode();
       gesture_pub_.publish(gesture_code_msg_);
       message_counter_ = 0;
-			average_gesture_code_ = 0;
     }
+  }
+
+  int getCode()
+  {
+    unsigned int max = 0;
+    int command = -1;
+    for (const auto& cmd_pair : command_history_)
+    {
+      if (cmd_pair.second > max)
+      {
+        command = cmd_pair.first;
+        max = cmd_pair.second;
+      }
+    }
+    command_history_.clear();
+    return command;
   }
 
   // Declare members
@@ -95,9 +111,9 @@ private:
   ros::Publisher gesture_pub_;
   std_msgs::Int16 gesture_code_msg_;
 
+  std::map<int, unsigned int> command_history_;
   unsigned int message_counter_;
-  const int max_count_;
-  double average_gesture_code_;
+  const unsigned int max_count_;
 };
 
 /*
