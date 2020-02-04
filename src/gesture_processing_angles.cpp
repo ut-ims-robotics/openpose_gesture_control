b87ae88019f4a1e9a7a085bf75264749f2ebd9be
Igor Rybalskii <gryogor918@gmail.com>
2020-02-04 13:59:36 +0200

    changed angle detection

--- src/gesture_processing_angles.cpp
+++ src/gesture_processing_angles.cpp
@@ -13,6 +13,29 @@ std::string command;
 int identifier;
 bool command_received = false;
 
+//arm check
+bool RArm = false;
+bool LArm = false;
+
+//declaring vecors for body parts
+//Right hand
+std::vector <float> Neck_RShoulder = {0.0, 0.0};
+std::vector <float> RElbow_RShoulder = {0.0, 0.0};
+std::vector <float> RShoulder_RElbow = {0.0, 0.0};
+std::vector <float> RWrist_RElbow = {0.0, 0.0};
+// Left  hand
+std::vector <float> Neck_LShoulder = {0.0, 0.0};
+std::vector <float> LElbow_LShoulder = {0.0, 0.0};
+std::vector <float> LShoulder_LElbow = {0.0, 0.0};
+std::vector <float> LWrist_LElbow = {0.0, 0.0};
+
+//Here all 4 angles are stored
+std::vector <float> angles = {0.0, 0.0, 0.0, 0.0};
+//Order: RShoulder, RElbow, LShoulder, LElbow
+
+//for later use
+std::vector <float> ref_angles_and_pose_name;
+
 std::vector <float> right_pose = {3.0, -1.25, 1000, 1000};
 std::vector <float> left_pose = {1000, 1000, 0.15, 1.5};
 std::vector <float> forward_pose = {4.3, -2.45, -1.25, 0.12};
@@ -44,62 +67,144 @@ void chatterCallback(const openpose_ros_msgs::OpenPoseHumanList& pose_msg)
     LElbow = pose_msg.human_list[0].body_key_points_with_prob[6];
     LWrist = pose_msg.human_list[0].body_key_points_with_prob[7];
 
-    //calculating points for angle of right shoulder
-    float Neck_y_for_R_shoulder = Neck.y - RShoulder.y;
-    float Neck_x_for_R_shoulder = Neck.x - RShoulder.x;
-    float R_Elbow_y_for_R_shoulder = RElbow.y - RShoulder.y;
-    float R_Elbow_x_for_R_shoulder = RElbow.x - RShoulder.x;
-
-    //Right shoulder angle 
-    float RSAngle1 = atan2(Neck_y_for_R_shoulder, Neck_x_for_R_shoulder); 
-    float RSAngle2 = atan2(R_Elbow_y_for_R_shoulder, R_Elbow_x_for_R_shoulder);
-    if (RSAngle2 < 0)
+    //finding all necessary vectors for right arm
+    if (RShoulder.x == 0.0 || RElbow.x == 0.0 || RWrist.x == 0.0)
     {
-      RSAngle2 = 3.14159 + (RSAngle2 + 3.14159);
+      RArm = false;
     }
-    RSAngle = RSAngle1 + RSAngle2;
-    ROS_INFO_STREAM("Right shoulder is ");
-    ROS_INFO_STREAM(RSAngle);
-    //Right shoulder angle done
+    else
+    {
+      RArm = true;
+      //0 - x    1 - y
+      //vectors for shoulder angle
+      Neck_RShoulder[0] = Neck.x - RShoulder.x;
+      Neck_RShoulder[1] = Neck.y - RShoulder.y;
+      RElbow_RShoulder[0] = RElbow.x - RShoulder.x;
+      RElbow_RShoulder[1] = RElbow.y - RShoulder.y;
+      //vectors for elbow angle
+      RShoulder_RElbow[0] = RShoulder.x - RElbow.x;
+      RShoulder_RElbow[1] = RShoulder.y - RElbow.y;
+      RWrist_RElbow[0] = RWrist.x - RElbow.x;
+      RWrist_RElbow[1] = RWrist.y - RElbow.y;
+    }
 
-    //calculating points for angle of right elbow
-    float R_shoulder_y_for_R_elbow = RShoulder.y - RElbow.y;
-    float R_shoulder_x_for_R_elbow = RShoulder.x - RElbow.x;
-    float R_wrist_y_for_R_elbow = RWrist.y - RElbow.y;
-    float R_wrist_x_for_R_elbow = RWrist.x - RElbow.x;
+    //finding all necessary vectors for left arm
+    if (LShoulder.x == 0.0 || LElbow.x == 0.0 || LWrist.x == 0.0)
+    {
+      LArm = false;
+    }
+    else
+    {
+      LArm = true;
+      //0 - x    1 - y
+      //vectors for shoulder angle
+      Neck_LShoulder[0] = Neck.x - LShoulder.x;
+      Neck_LShoulder[1] = Neck.y - LShoulder.y;
+      LShoulder_LElbow[0] = LElbow.x - LShoulder.x;
+      LShoulder_LElbow[1] = LElbow.y - LShoulder.y;
+      //vectors for elbow angle
+      LElbow_LShoulder[0] = LShoulder.x - LElbow.x;
+      LElbow_LShoulder[1] = LShoulder.y - LElbow.y;
+      LWrist_LElbow[0] = LWrist.x - LElbow.x;
+      LWrist_LElbow[1] = LWrist.y - LElbow.y;
+    }
 
-    //Right elbow angle
-    float REAngle1 = atan2(R_shoulder_y_for_R_elbow, R_shoulder_x_for_R_elbow);
-    float REAngle2 = atan2(R_wrist_y_for_R_elbow, R_wrist_x_for_R_elbow);
-    REAngle = REAngle2 - REAngle1;
-    ROS_INFO_STREAM("Right elbow is ");
-    ROS_INFO_STREAM(REAngle);
-    //Right elbow angle done
+    //calculating right arm angles
+    if (!RArm)
+    {
+      angles[0] = 0.0;
+      angles[1] = 0.0;
+    }
+    else
+    {
+      float Neck_RShoulder_magnitude = sqrt(pow(Neck_RShoulder[0],2) + pow(Neck_RShoulder[1],2));
+      float RElbow_RShoulder_magnitude = sqrt(pow(RElbow_RShoulder[0],2) + pow(RElbow_RShoulder[1],2));
+      angles[0] = acos((Neck_RShoulder[0]*LShoulder_LElbow[0] + Neck_RShoulder[1]*LShoulder_LElbow[1])/(Neck_RShoulder_magnitude*RElbow_RShoulder_magnitude));
 
-    //calculating points for angle of left shoulder
-    float Neck_y_for_L_shoulder = LShoulder.y - Neck.y; //right now this and lower line have incorrect naming of variable
-    float Neck_x_for_L_shoulder = LShoulder.x - Neck.x;
-    float L_Elbow_y_for_L_shoulder = LElbow.y - LShoulder.y;
-    float L_Elbow_x_for_L_shoulder = LElbow.x - LShoulder.x;
+      float RShoulder_RElbow_magnitude = sqrt(pow(RShoulder_RElbow[0],2) + pow(RShoulder_RElbow[1],2));
+      float RWrist_RElbow_magnitude = sqrt(pow(RWrist_RElbow[0],2) + pow(RWrist_RElbow[1],2));
+      angles[1] = acos((RShoulder_RElbow[0]*RWrist_RElbow[0] + RShoulder_RElbow[1]*RWrist_RElbow[1])/(RShoulder_RElbow_magnitude*RWrist_RElbow_magnitude));
+    }
 
-    //Left shoulder angle
-    float LSAngle1 = atan2(Neck_y_for_L_shoulder, Neck_x_for_L_shoulder);
-    float LSAngle2 = atan2(L_Elbow_y_for_L_shoulder, L_Elbow_x_for_L_shoulder);
-    LSAngle = LSAngle1 + LSAngle2;
-    ROS_INFO_STREAM("Left shoulder is ");
-    ROS_INFO_STREAM(LSAngle);
-    //Left shoulder angle done
+    //calculating left arm angles
+    if (!LArm)
+    {
+      angles[2] = 0.0;
+      angles[3] = 0.0;
+    }
+    else
+    {
+      float Neck_LShoulder_magnitude = sqrt(pow(Neck_LShoulder[0],2) + pow(Neck_LShoulder[1],2));
+      float LElbow_LShoulder_magnitude = sqrt(pow(LElbow_LShoulder[0],2) + pow(LElbow_LShoulder[1],2));
+      angles[2] = acos((Neck_LShoulder[0]*LShoulder_LElbow[0] + Neck_LShoulder[1]*LShoulder_LElbow[1])/(Neck_LShoulder_magnitude*LElbow_LShoulder_magnitude));
 
-    //calculating points for angle of left elbow
-    float L_wrist_y_for_L_elbow = LWrist.y - LElbow.y;
-    float L_wrist_x_for_L_elbow = LWrist.x - LElbow.x;
+      float LShoulder_LElbow_magnitude = sqrt(pow(LShoulder_LElbow[0],2) + pow(LShoulder_LElbow[1],2));
+      float LWrist_LElbow_magnitude = sqrt(pow(LWrist_LElbow[0],2) + pow(LWrist_LElbow[1],2));
+      angles[3] = acos((LShoulder_LElbow[0]*LWrist_LElbow[0] + LShoulder_LElbow[1]*LWrist_LElbow[1])/(LShoulder_LElbow_magnitude*LWrist_LElbow_magnitude)); 
+    }
+    std::cout << "RShoulder: " << angles[0] << std::endl << "RElbow: " << angles[1] << std::endl << "LShoulder: " << angles[2] << std::endl << "LElbow: " << angles[3] << std::endl;
+    
+    
 
-    //Left elbow angle
-    float LEAngle1 = atan2(L_Elbow_y_for_L_shoulder, L_Elbow_x_for_L_shoulder);
-    float LEAngle2 = atan2(L_wrist_y_for_L_elbow, L_wrist_x_for_L_elbow);
-    LEAngle = LEAngle1 - LEAngle2;
-    ROS_INFO_STREAM("Left elbow is ");
-    ROS_INFO_STREAM(LEAngle);
+
+    // prev version
+
+    // //calculating points for angle of right shoulder
+    // float Neck_y_for_R_shoulder = Neck.y - RShoulder.y;
+    // float Neck_x_for_R_shoulder = Neck.x - RShoulder.x;
+    // float R_Elbow_y_for_R_shoulder = RElbow.y - RShoulder.y;
+    // float R_Elbow_x_for_R_shoulder = RElbow.x - RShoulder.x;
+
+    // //Right shoulder angle 
+    // float RSAngle1 = atan2(Neck_y_for_R_shoulder, Neck_x_for_R_shoulder); 
+    // float RSAngle2 = atan2(R_Elbow_y_for_R_shoulder, R_Elbow_x_for_R_shoulder);
+    // if (RSAngle2 < 0)
+    // {
+    //   RSAngle2 = 3.14159 + (RSAngle2 + 3.14159);
+    // }
+    // RSAngle = RSAngle1 + RSAngle2;
+    // ROS_INFO_STREAM("Right shoulder is ");
+    // ROS_INFO_STREAM(RSAngle);
+    // //Right shoulder angle done
+
+    // //calculating points for angle of right elbow
+    // float R_shoulder_y_for_R_elbow = RShoulder.y - RElbow.y;
+    // float R_shoulder_x_for_R_elbow = RShoulder.x - RElbow.x;
+    // float R_wrist_y_for_R_elbow = RWrist.y - RElbow.y;
+    // float R_wrist_x_for_R_elbow = RWrist.x - RElbow.x;
+
+    // //Right elbow angle
+    // float REAngle1 = atan2(R_shoulder_y_for_R_elbow, R_shoulder_x_for_R_elbow);
+    // float REAngle2 = atan2(R_wrist_y_for_R_elbow, R_wrist_x_for_R_elbow);
+    // REAngle = REAngle2 - REAngle1;
+    // ROS_INFO_STREAM("Right elbow is ");
+    // ROS_INFO_STREAM(REAngle);
+    // //Right elbow angle done
+
+    // //calculating points for angle of left shoulder
+    // float Neck_y_for_L_shoulder = LShoulder.y - Neck.y; //right now this and lower line have incorrect naming of variable
+    // float Neck_x_for_L_shoulder = LShoulder.x - Neck.x;
+    // float L_Elbow_y_for_L_shoulder = LElbow.y - LShoulder.y;
+    // float L_Elbow_x_for_L_shoulder = LElbow.x - LShoulder.x;
+
+    // //Left shoulder angle
+    // float LSAngle1 = atan2(Neck_y_for_L_shoulder, Neck_x_for_L_shoulder);
+    // float LSAngle2 = atan2(L_Elbow_y_for_L_shoulder, L_Elbow_x_for_L_shoulder);
+    // LSAngle = LSAngle1 + LSAngle2;
+    // ROS_INFO_STREAM("Left shoulder is ");
+    // ROS_INFO_STREAM(LSAngle);
+    // //Left shoulder angle done
+
+    // //calculating points for angle of left elbow
+    // float L_wrist_y_for_L_elbow = LWrist.y - LElbow.y;
+    // float L_wrist_x_for_L_elbow = LWrist.x - LElbow.x;
+
+    // //Left elbow angle
+    // float LEAngle1 = atan2(L_Elbow_y_for_L_shoulder, L_Elbow_x_for_L_shoulder);
+    // float LEAngle2 = atan2(L_wrist_y_for_L_elbow, L_wrist_x_for_L_elbow);
+    // LEAngle = LEAngle1 - LEAngle2;
+    // ROS_INFO_STREAM("Left elbow is ");
+    // ROS_INFO_STREAM(LEAngle);
   }
 }
 
@@ -110,31 +215,10 @@ int main(int argc, char **argv)
   ros::Subscriber sub = nh.subscribe("/openpose_ros/human_list", 1000, chatterCallback);
   ros::AsyncSpinner spinner(4);
 	  spinner.start();
-  return 0;
   while (ros::ok)
   {
-//Need to be changed (wrong)
-    float left_pose_s, right_pose_s, forward_pose_s, stop_pose_s;
-    int None_values_cnt_right = 0;
-    for (float point: right_pose)
-    {
-      if (point != 1000)
-      {
-        if (point < RSAngle)
-        {
-          right_pose_s += RSAngle - point; 
-        }
-        else
-        {
-          right_pose_s += point - RSAngle;
-        }
-      }
-      else
-      {
-        None_values_cnt_right += 1;
-      }
-      
-      }
-    }
+    ROS_INFO_STREAM("Working...");
+    ros::Duration(0.05).sleep;
   }
+  return 0;
 }
